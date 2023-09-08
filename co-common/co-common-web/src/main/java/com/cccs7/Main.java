a7e52a05895d12dd64f69984ab4a3874bab5df7d
cs7eric <Cs7eric@outlook.com>
2023-09-08 12:30:09 +0800

    feat:统一管理

---
+++ co-common/co-common-web/src/main/java/com/cccs7/Main.java
@@ -0,0 +1,14 @@
+package com.cccs7;
+
+/**
+ * <p> $END </p>
+ *
+ * @Author cccs7/cs7eric - csq020611@gmail.com
+ * @Description $END
+ * @Date 2023/9/8 12:25
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
