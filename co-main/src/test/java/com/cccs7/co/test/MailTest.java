84e1b9e8a602cf78997e937e900d3cb5ed5a0af3
cs7eric <Cs7eric@outlook.com>
2023-09-30 11:46:53 +0800

    feat/co-test:集成测试

--- co-main/src/test/java/com/cccs7/co/test/MailTest.java
+++ co-main/src/test/java/com/cccs7/co/test/MailTest.java
@@ -1,31 +1,24 @@
 package com.cccs7.co.test;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.stereotype.Component;
 
 /**
- * <p> 邮箱测试类 </p>
+ * <p> 邮件服务测试类 </p>
  *
  * @Author cccs7/cs7eric - csq020611@gmail.com
- * @Description 邮箱测试类
- * @Date 2023/9/30 11:08
+ * @Description 邮件服务测试类
+ * @Date 2023/9/30 11:35
  */
-
+@SpringBootTest
 public class MailTest {
 
-    @Autowired
-    JavaMailSenderImpl mailSender;
 
     @Test
-    private void sendMessage() {
-        String emailServiceCode = "E34DF9";
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setSubject("注册验证码");
-        message.setText("注册验证码是" + emailServiceCode);
-        message.setFrom("csq020611@gmail.com");
-        mailSender.send(message);
+    public void sendMessage() {
+        System.out.println("this is a mail test");
+
     }
 }
