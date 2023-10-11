58d885f2c9d21f74e0e171bdbe597dd3204d9a15
byxl8112 <byxl8112@gmail.com>
2023-10-11 13:41:16 +0800

    test: git pr

--- co-main/src/main/java/com/cccs7/co/controller/ArticleController.java
+++ co-main/src/main/java/com/cccs7/co/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.cccs7.co.controller;
 import com.cccs7.co.bean.entity.Article;
 import com.cccs7.co.service.ArticleService;
 import com.cccs7.web.bean.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import java.util.List;
  * @Description ArticleController
  * @Date 2023/10/9 10:43
  */
+@Slf4j
 @RestController
 @RequestMapping("/articles")
 public class ArticleController {
@@ -30,7 +32,7 @@ public class ArticleController {
             throw new RuntimeException("not null of id");
         }
         Article article = articleService.getArticleById(id);
-        System.out.println(article);
+        log.info("article:{}",article);
         return Result.ok(article);
     }
 
