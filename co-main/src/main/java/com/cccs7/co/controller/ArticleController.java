41c25351c52c32eeb7f595a17713731c3db58277
byxl8112 <byxl8112@gmail.com>
2023-10-11 16:53:02 +0800

    test: git pr

--- co-main/src/main/java/com/cccs7/co/controller/ArticleController.java
+++ co-main/src/main/java/com/cccs7/co/controller/ArticleController.java
@@ -34,7 +34,7 @@ public class ArticleController {
         }
         Article article = articleService.getArticleById(id);
         if (Objects.isNull(article)) {
-            return Result.ok("该a更改不存在");
+            return Result.ok("该a更改分发不存在");
         }
         log.info("article:{}",article);
         return Result.ok(article);
